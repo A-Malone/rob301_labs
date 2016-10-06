38b2c14d72e590732af1758553ed1f3941579e3f
A-Malone <a_s_malone@hotmail.com>
2016-10-05 23:03:45 -0400

    Initial commit

---
+++ lab3/src/lab3/BangBang.java
@@ -0,0 +1,12 @@
+package lab3;
+
+import lejos.hardware.Button;
+
+public class BangBang
+{
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Hello ROB301!");
+        Button.waitForAnyPress();
+    }
+}
